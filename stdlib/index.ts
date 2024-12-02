37983f36665efe90fea0aa8fbd4c5b3eca13be38
JSON <hutchisonjayson@gmail.com>
2024-12-02 13:34:00 +0300

    Organized files

--- stdlib/index.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    console: console,
-    error: function(msg: string) {
-        console.error("Error: " + msg);
-    },
-    createArray: function() {
-        return ([]);
-    }
-} as any;
